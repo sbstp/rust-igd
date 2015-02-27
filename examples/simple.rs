89472475f441783974456a964fc87e86e8f5d559
Simon Bernier St-Pierre <sbernierstpierre@gmail.com>
2015-02-27 02:00:59 -0500

    Add simple example.

---
+++ examples/simple.rs
@@ -0,0 +1,15 @@
+extern crate igd;
+
+fn main() {
+    match igd::search_gateway() {
+        Err(ref err) => println!("{:?}", err),
+        Ok(local_soaddr) => {
+            match igd::get_external_ip(local_soaddr) {
+                Err(ref err) => println!("{:?}", err),
+                Ok(ext_addr) => {
+                    println!("Local gateway: {}, Extern gateway: {}", local_soaddr, ext_addr);
+                },
+            }
+        },
+    }
+}
