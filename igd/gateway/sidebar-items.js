b8a6c1a9f24f325b94929f4e24820b6d4b84be23
Simon Bernier St-Pierre <sbernierstpierre@gmail.com>
2015-10-30 00:18:33 -0400

    Update docs

---
+++ igd/gateway/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["PortMappingProtocol","Represents the protocols available for port mapping."],["RequestError","Errors that can occur when sending the request to the gateway."]],"struct":[["Gateway","This structure represents a gateway found by the search functions."]]});
