4851fbeea324624dc1c02a2b64c3ccd705fff069
Simon Bernier St-Pierre <sbernierstpierre@gmail.com>
2016-02-08 00:10:37 -0500

    Update docs

--- implementors/core/str/pattern/trait.TwoWayStrategy.js
+++ implementors/core/str/pattern/trait.TwoWayStrategy.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['bitflags'] = [];implementors['libc'] = [];implementors['regex_syntax'] = [];implementors['hyper'] = [];implementors['igd'] = [];
+implementors['libc'] = [];implementors['regex_syntax'] = [];implementors['bitflags'] = [];implementors['hyper'] = [];implementors['igd'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
