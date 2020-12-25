6594429b2a733b50ffd134a71ded5faa31eda69f
Artem Vorotnikov <artem@vorotnikov.me>
2020-12-25 04:34:48 +0300

    rand 0.8

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -7,5 +7,5 @@ pub use self::options::SearchOptions;
 use rand::{self, Rng};
 
 pub fn random_port() -> u16 {
-    rand::thread_rng().gen_range(32_768_u16, 65_535_u16)
+    rand::thread_rng().gen_range(32_768_u16..65_535_u16)
 }
